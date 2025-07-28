c61f82d26f6468bf03bedae798e17716e84ed85f
GoobaGaba <gabrielleunda@gmail.com>
2025-07-28 22:21:02 +0000

    no i just reuploaded the photo. its called logo.png

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   // Using the correct brand image for the logo.
   return (
     <Image
-      src="/Landhare Couple v2.png"
+      src="/logo.png"
       alt="LandHare Logo"
       width={128}
       height={128}
