8532e70e232e7e3d4ad221053ad65fbece723999
GoobaGaba <gabrielleunda@gmail.com>
2025-10-05 17:58:30 +0000

    images under listings are showing the same bug from homepage and browse

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
       }
     ],
   },
